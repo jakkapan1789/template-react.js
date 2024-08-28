4f09232e7bd69839bb567641c22e690abad7185b
Jakkapan Pakeerat <109788212+jakkapan1789@users.noreply.github.com>
2024-08-28 14:07:50 +0700

    Update MainMenu.js

--- src/components/Header/MainMenu/MainMenu.js
+++ src/components/Header/MainMenu/MainMenu.js
@@ -1,68 +1,12 @@
-// import navigationConfig from "data/navigation";
-// import { useNavigate } from "react-router";
-// import { useState } from "react";
-
-// export default function MainMenu() {
-//   const navigate = useNavigate();
-//   const [visibleSubmenu, setVisibleSubmenu] = useState(null);
-
-//   const handleMenuClick = (navPath) => {
-//     setVisibleSubmenu(null);
-//     if (navPath) {
-//       setTimeout(() => navigate(navPath), 0);
-//     }
-//   };
-
-//   return (
-//     <nav className="navbar">
-//       <ul className="menu">
-//         {navigationConfig.map((nav, index) => (
-//           <li
-//             className="dropdown"
-//             key={index}
-//             onMouseEnter={() => setVisibleSubmenu(index)}
-//             onMouseLeave={() => setVisibleSubmenu(null)}
-//           >
-//             <a
-//               onClick={() => nav.path && handleMenuClick(nav.path)}
-//               style={{ cursor: "pointer" }}
-//             >
-//               {nav.label}
-//             </a>
-//             {nav.submenu && (
-//               <div
-//                 className="submenu"
-//                 style={{
-//                   display: visibleSubmenu === index ? "block" : "none",
-//                 }}
-//               >
-//                 {nav.submenu.map((sub, subIndex) => (
-//                   <div key={subIndex}>
-//                     <a
-//                       onClick={() => handleMenuClick(sub.path)}
-//                       style={{ cursor: "pointer" }}
-//                     >
-//                       {sub.label}
-//                     </a>
-//                   </div>
-//                 ))}
-//               </div>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// }
-
 import navigationConfig from "data/navigation";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function MainMenu() {
   const navigate = useNavigate();
   const [visibleSubmenu, setVisibleSubmenu] = useState(null);
   const [visibleSubSubmenu, setVisibleSubSubmenu] = useState(null);
+  const submenuRefs = useRef({});
 
   const handleMenuClick = (navPath) => {
     setVisibleSubmenu(null);
@@ -80,14 +24,25 @@ export default function MainMenu() {
             className="dropdown"
             key={index}
             onMouseEnter={() => setVisibleSubmenu(index)}
-            onMouseLeave={() => setVisibleSubmenu(null)}
+            onMouseLeave={() => {
+              setVisibleSubmenu(null);
+              setVisibleSubSubmenu(null);
+            }}
           >
             <a
               onClick={() => nav.path && handleMenuClick(nav.path)}
               style={{ cursor: "pointer" }}
             >
               {nav.label}
+              {nav.submenu && (
+                <i
+                  className={`arrow ${
+                    visibleSubmenu === index ? "down" : "up"
+                  }`}
+                ></i>
+              )}
             </a>
+
             {nav.submenu && (
               <div
                 className="submenu"
@@ -99,6 +54,9 @@ export default function MainMenu() {
                   <div
                     key={subIndex}
                     className="dropdown"
+                    ref={(el) => {
+                      submenuRefs.current[`${index}-${subIndex}`] = el;
+                    }}
                     onMouseEnter={() => setVisibleSubSubmenu(subIndex)}
                     onMouseLeave={() => setVisibleSubSubmenu(null)}
                   >
@@ -107,6 +65,13 @@ export default function MainMenu() {
                       style={{ cursor: "pointer" }}
                     >
                       {sub.label}
+                      {sub.submenu && (
+                        <i
+                          className={`arrow ${
+                            visibleSubSubmenu === subIndex ? "right" : "down"
+                          }`}
+                        ></i>
+                      )}
                     </a>
                     {sub.submenu && (
                       <div
@@ -114,6 +79,9 @@ export default function MainMenu() {
                         style={{
                           display:
                             visibleSubSubmenu === subIndex ? "block" : "none",
+                          top:
+                            submenuRefs.current[`${index}-${subIndex}`]
+                              ?.offsetTop || 0,
                         }}
                       >
                         {sub.submenu.map((subSub, subSubIndex) => (
@@ -138,79 +106,3 @@ export default function MainMenu() {
     </nav>
   );
 }
-
-// import React, { useState } from "react";
-
-// const MainMenu = () => {
-//   const [htmlCssSubMenuOpen, setHtmlCssSubMenuOpen] = useState(false);
-//   const [moreSubMenuOpen, setMoreSubMenuOpen] = useState(false);
-
-//   return (
-//     <nav className="navbar">
-//       <ul className="links">
-//         <li
-//           onMouseEnter={() => setHtmlCssSubMenuOpen(true)}
-//           onMouseLeave={() => setHtmlCssSubMenuOpen(false)}
-//         >
-//           <a href="#">HTML & CSS</a>
-//           <i
-//             className={`bx bxs-chevron-down arrow ${
-//               htmlCssSubMenuOpen ? "rotate" : ""
-//             }`}
-//           />
-//           {htmlCssSubMenuOpen && (
-//             <ul className="htmlCss-sub-menu sub-menu">
-//               <li>
-//                 <a href="#">Web Design</a>
-//               </li>
-//               <li>
-//                 <a href="#">Login Forms</a>
-//               </li>
-//               <li>
-//                 <a href="#">Card Design</a>
-//               </li>
-//               <li
-//                 className="more"
-//                 onMouseEnter={() => setMoreSubMenuOpen(true)}
-//                 onMouseLeave={() => setMoreSubMenuOpen(false)}
-//               >
-//                 <span>
-//                   <a href="#">More</a>
-//                   <i
-//                     className={`bx bxs-chevron-right arrow ${
-//                       moreSubMenuOpen ? "rotate" : ""
-//                     }`}
-//                   />
-//                 </span>
-//                 {moreSubMenuOpen && (
-//                   <ul className="more-sub-menu sub-menu">
-//                     <li>
-//                       <a href="#">Neumorphism</a>
-//                     </li>
-//                     <li>
-//                       <a href="#">Pre-loader</a>
-//                     </li>
-//                     <li>
-//                       <a href="#">Glassmorphism</a>
-//                     </li>
-//                   </ul>
-//                 )}
-//               </li>
-//             </ul>
-//           )}
-//         </li>
-//         <li>
-//           <a href="#">JAVASCRIPT</a>
-//         </li>
-//         <li>
-//           <a href="#">ABOUT US</a>
-//         </li>
-//         <li>
-//           <a href="#">CONTACT US</a>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default MainMenu;
